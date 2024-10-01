1c1f7ca5725730b89204dc3a85868f89b8533946
Avina <mauryavina20@gmail.com>
2024-10-01 11:57:45 -0400

    admin css new

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-c!)uvk1_!k8$9@6ef6sjrmoi5ysd2cg=h@kz&2-&n5)1!_!##r'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['django-env2.eba-gmieun3p.us-east-1.elasticbeanstalk.com']
 
@@ -117,7 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = 'static'
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
